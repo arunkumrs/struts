5c216f2baa409404977e1057f427546e35376387
arunkumrs <arun.kumrs@yahoo.com>
2018-10-23 10:10:16 -0600

    Un strutted

---
+++ src/main/java/org/superbiz/struts/UsersController.java
@@ -0,0 +1,53 @@
+package org.superbiz.struts;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UsersController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/addUser")
+    public String addUserForm() {
+        return "addUserForm";
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(long id, String firstName, String lastName) {
+        User user = new User();
+        user.setId(id);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        userRepository.save(user);
+        return "addedUser";
+    }
+
+    @GetMapping("/findUser")
+    public String findUserForm() {
+        return "findUserForm";
+    }
+
+    @PostMapping("/findUser")
+    public String findUser(@RequestParam long id, Model model) {
+        User user = userRepository.findOne(id);
+
+        if (user == null) {
+            model.addAttribute("errorMessage", "User not found");
+            return "findUserForm";
+        }
+
+        model.addAttribute("user", user);
+        return "displayUser";
+    }
+
+    @GetMapping("/list")
+    public String listUsers(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "displayUsers";
+    }
+}
